e0f1f00d5372bfe766b6fe782381337d95d431ec
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-26 12:14:20 -0500

    refactored workers from models

--- models/symbol.py
+++ models/symbol.py
@@ -16,10 +16,9 @@ class Symbol(BaseModel):
 	pe_ratio: float
 	close_price: float
 	prev_close: float
-	position: str #= "gain" if close_price > prev_close else "loss"
-	close_diff: float #= prev_close - close_price
-	close_diff_percent: float #= - round((close_diff/prev_close)*100, 2) if close_diff < 0 \
-								#else round((close_diff/prev_close)*100, 2)
+	position: str
+	close_diff: float
+	close_diff_percent: float
 	day_low: float
 	day_high: float
 	year_low: float
