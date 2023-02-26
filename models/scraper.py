1a7eceddab737fc19d0c293ceac8f283e1ce0b70
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-26 00:24:00 -0500

    extracted data

--- models/scraper.py
+++ models/scraper.py
@@ -1,7 +1,9 @@
+import re
 import asyncio
+import aiohttp
+from datetime import date
 from threading import Thread
 
-import requests
 from bs4 import BeautifulSoup
 
 class Scraper(Thread):
@@ -13,14 +15,45 @@ class Scraper(Thread):
 		self._exchange = exchange.upper()
 		self._url = f"https://www.google.com/finance/quote/{self._symbol}:{self._exchange}?hl=en"
 
-	@staticmethod
+
 	async def get_stock_data(self):
+		async with aiohttp.ClientSession() as session:
+			async with session.get(self._url) as response:
+				if response.status != 200:
+					raise Exception("Unable to retrieve response from request!")
+
+				page_contents = BeautifulSoup(await response.text(), "html.parser")
+
+				#table_data = page_contents.find_all("div", {"class": "P6K39c"})
+				table_data = page_contents.select(".P6K39c")
+				table_keys = ["prev_close", "day_low", "day_high", "year_low", "year_high", "market_cap", "avg_volume", "pe_ratio", "dividend_yield"]
+
+				values = ",".join(( data.text for data in table_data ))
+
+				# extract only numeric values
+				pattern = r'\d+(?:\.\d+)?'
+				parsed_values = re.findall(pattern, values)
+
+				# convert numeric strings to float and create dict
+				table_values = [ float(val) for val in parsed_values ][0:9]
+				params = dict(zip(table_keys, table_values))
+
+				return params
+
+
+
+	async def run(self):
 		pass
 
-	def run(self):
-		asyncio.run(self.get_stock_data())
 
+async def main():
+
+	symbol = "msft"
+	worker = Scraper(symbol)
+
+	results = await worker.get_stock_data()
+	print(results)
 
 
 if __name__ == "__main__":
-    pass
+	asyncio.run(main())
--- models/symbol.py
+++ models/symbol.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import date
 
 from pydantic import BaseModel, Field
 
@@ -6,12 +7,24 @@ from pydantic import BaseModel, Field
 class Symbol(BaseModel):
 	symbol: str
 	name: str
-	exchange: str
+	date_scraped: date
+	exchange: str = "NASDAQ"
 	price: float
+	market_cap: float = Field(
+		description="market cap in trillions (USD)"
+	)
+	pe_ratio: float
+	close_price: float
 	prev_close: float
+	position: str = "gain" if close_price > prev_close else "loss"
+	close_diff: float = prev_close - close_price
+	close_diff_percent: float = - round((close_diff/prev_close)*100, 2) if close_diff < 0 \
+								else round((close_diff/prev_close)*100, 2)
+	day_low: float
+	day_high: float
+	year_low: float
+	year_high: float
 	dividend_yield: Optional[float]
 	avg_volume: float = Field(
 		description="avg trading volume in millions"
 	)
-
-
