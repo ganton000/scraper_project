726da576e958850ae57fba86abe16a4af16f5f28
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-26 11:42:14 -0500

    refactored class

--- models/scraper.py
+++ models/scraper.py
@@ -3,20 +3,22 @@ import asyncio
 import aiohttp
 from datetime import date
 from threading import Thread
+from typing import Optional, queue
 
 from bs4 import BeautifulSoup
 
-class Scraper(Thread):
+class GoogleFinanceWorker(Thread):
 
 
-	def __init__(self, symbol: str, exchange: str="NASDAQ", **kwargs) -> None:
-		super(Scraper, self).__init__()
+	def __init__(self, symbol: str, output_queue: queue, exchange: str="NASDAQ", **kwargs) -> None:
+		super(GoogleFinanceWorker, self).__init__()
 		self._symbol = symbol.upper()
 		self._exchange = exchange.upper()
+		self._output_queue = output_queue
 		self._url = f"https://www.google.com/finance/quote/{self._symbol}:{self._exchange}?hl=en"
 
 
-	async def get_stock_data(self):
+	async def _get_stock_data(self):
 		async with aiohttp.ClientSession() as session:
 			async with session.get(self._url) as response:
 				if response.status != 200:
@@ -24,6 +26,21 @@ class Scraper(Thread):
 
 				page_contents = BeautifulSoup(await response.text(), "html.parser")
 
+				return page_contents
+
+	async def run(self):
+		pass
+
+class GoogleFinanceProcessor(Thread):
+
+	def __init__(self, input_queue: queue, output_queue: queue, **kwargs) -> None:
+		super(GoogleFinanceProcessor, self).__init__()
+		self._input_queue = input_queue
+		self._output_queue = output_queue
+
+
+	async def process_data(self):
+				page_contents = self._get_stock_data()
 				#table_data = page_contents.find_all("div", {"class": "P6K39c"})
 				table_data = page_contents.select(".P6K39c")
 				table_keys = ["prev_close", "day_low", "day_high", "year_low", "year_high", "market_cap", "avg_volume", "pe_ratio", "dividend_yield"]
@@ -42,17 +59,19 @@ class Scraper(Thread):
 				return params
 
 
-
 	async def run(self):
 		pass
 
 
 async def main():
 
-	symbol = "msft"
-	worker = Scraper(symbol)
+	symbol = "tsla"
+	worker = GoogleFinanceWorker(symbol)
 
-	results = await worker.get_stock_data()
+	results = asyncio.create_task(worker.get_stock_data())
+
+
+	await results
 	print(results)
 
 
