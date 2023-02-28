4be17f1b37747e41b93b6b56211714265b16ac14
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-27 22:44:40 -0500

    added api call to stock data

--- frontend/src/components/Stocks.js
+++ frontend/src/components/Stocks.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Stocks = ({ props }) => {
-    return <div>Stocks</div>;
+import { api } from "../apis/api";
+
+
+const Stocks = () => {
+    const [stockData, setStockData] = useState([]);
+
+    useEffect(() => {
+
+        const fetchStocks = async () => {
+            try {
+                const response = await api.get("/symbols");
+                console.log(response);
+                setStockData(response.data);
+
+            } catch(error) {
+                console.error(error);
+            }
+        };
+
+        fetchStocks();
+    }, []);
+
+    return stockData.length ? (
+        stockData.map((stock, idx) => {
+            return (
+                <div className="stock-item" key={idx}>
+                    {stock.name}, {stock.price}, {stock.day_low},{" "}
+                    {stock.day_high}
+                </div>
+            );
+        })
+    ) : (
+        <div> Fetching data... </div>
+    );
 };
 
 export default Stocks;
---
+++ frontend/src/helpers/actions.js
@@ -0,0 +1,7 @@
+//import api from '../apis/api';
+
+//export const fetchStocks = async () => {
+//	const response = await api.get("/symbols");
+//	console.log(response)
+//	return response.data()
+//}
