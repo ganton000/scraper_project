4d4dd62b4af245a7a726edecf59aa7f7cee8eaa0
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-28 19:55:36 -0500

    added put route

--- routes/stocks.py
+++ routes/stocks.py
@@ -7,7 +7,7 @@ from workers.GoogleWorker import GoogleFinanceWorker
 
 
 ## stub data
-symbols = ["AAPL", "TSLA", "MSFT"]
+symbols = ["AAPL", "TSLA", "MSFT", "GOOG"]
 
 STUB_DATA = []
 
@@ -38,22 +38,40 @@ async def get_all_symbols_with_pagination(start: int, limit: int) -> list[Symbol
 
 router = APIRouter()
 
-@router.get("/symbol/{symbol_name}", response_model=Symbol)
-async def get_symbol(symbol_name: str) -> Symbol:
-	symbol = await create_symbol(symbol_name)
-	return symbol
-
-@router.get("/symbols", response_model=MultipleSymbols)
-async def get_all_symbols(start: int=0, limit: int=2) -> MultipleSymbols:
-    symbols = await get_all_symbols_with_pagination(start, limit)
-    formatted_symbols = { "symbols": symbols }
-    symbols_response = MultipleSymbols(**formatted_symbols)
-    print(len(STUB_DATA))
-    return symbols_response
-
 @router.post("/symbol", response_model=MultipleSymbols)
 async def add_symbol(symbol_name: str) -> MultipleSymbols:
 	await create_stub_data()
 	new_data = await create_symbol(symbol_name)
 	STUB_DATA.append(new_data)
 	return STUB_DATA
+
+@router.get("/symbol/{symbol_name}", response_model=Symbol)
+async def get_symbol(symbol_name: str) -> Symbol:
+	symbol = await create_symbol(symbol_name)
+	return symbol
+
+@router.put("/symbol/{symbol_name}")
+async def update_symbol(new_symbol_data: dict) -> MultipleSymbols:
+	global STUB_DATA
+
+	STUB_DATA = [] ## reset
+
+	await create_stub_data()
+
+	symbol_name = new_symbol_data.get("symbol")
+	if symbol_name is None:
+		raise ValueError("Name property not defined")
+
+	filtered_stub_data = [ model for model in STUB_DATA if model.symbol != symbol_name]
+	filtered_stub_data.append(Symbol(**new_symbol_data))
+	formatted_symbols = { "symbols": filtered_stub_data }
+	symbols_response = MultipleSymbols(**formatted_symbols)
+	return symbols_response
+
+@router.get("/symbols", response_model=MultipleSymbols)
+async def get_all_symbols(start: int=0, limit: int=4) -> MultipleSymbols:
+    symbols = await get_all_symbols_with_pagination(start, limit)
+    formatted_symbols = { "symbols": symbols }
+    symbols_response = MultipleSymbols(**formatted_symbols)
+    return symbols_response
+
